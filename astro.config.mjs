bdf6dc6a1ab8b4b13b97f654c87b4728e12b3109
123456 <hoochanlon@outlook.com>
2025-12-02 20:48:56 +0800

    asd

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,46 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import expressiveCode from 'astro-expressive-code';
+import rehypeExternalLinks from "rehype-external-links";
+import markdoc from '@astrojs/markdoc';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+      markdown: {
+    rehypePlugins: [
+      [
+        rehypeExternalLinks,
+        {
+          target: "\_blank",
+          rel: ["noopener", "noreferrer", "external"],
+        },
+      ],
+    ],
+  },
+    integrations: [expressiveCode(), starlight({
+        title: 'My Docs',
+        social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+        editLink: {
+            baseUrl: 'https://github.com/hoochanlon/starlight-notes/edit/master',
+        },
+        sidebar: [
+            {
+                label: 'Astro-Strlight',
+                items: [
+                    // Each item here is one entry in the navigation menu.
+                    { label: 'Astro-Strlight 简要配置', slug: 'astro-starlight/astro-starlight-example' },
+                    { label: 'Astro-Strlight 写作脚本', slug: '20251202175510' },
+                ],
+            },
+            {
+                label: 'Astro-Paper',
+                items: [
+                    // Each item here is one entry in the navigation menu.
+                    { label: 'Astro-Paper 简要配置', slug: 'astro-paper/astro-paper-example' },
+                    { label: 'Astro-Paper 写作脚本', slug: 'astro-paper/astro-paper-scripts' },
+                ],
+            },
+        ],
+        }), markdoc()],
 });
